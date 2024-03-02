7e4dc933702b0bfd9574da8bc553a2440e8fdc45
cherry-1312 <157616113+cherry-1312@users.noreply.github.com>
2024-03-02 15:52:27 +0000

    Update features_cleaning.py

--- Main Codes/features_cleaning.py
+++ Main Codes/features_cleaning.py
@@ -38,3 +38,28 @@ df_features_filtered = df_features.drop(columns=columns_above_threshold)
 df_features_filtered = pd.concat([stock_ids, df_features_filtered], axis=1)
 
 df_features_filtered.to_csv('extracted_features_filtered.csv', index=False)
+
+# the following code chunk removes all columns unrelated 
+# to closing price
+
+prefix_to_keep = "Close__"
+
+
+selected_columns = [col for col in df.columns if col.startswith(prefix_to_keep)]
+
+
+df = df[selected_columns]
+
+
+columns_to_drop = []
+
+
+for column in df.columns:
+   unique_values = df[column].unique()
+   if len(unique_values) == 2 and 0 in unique_values and 1 in unique_values:
+       columns_to_drop.append(column)
+
+
+df = df.drop(columns=columns_to_drop)
+
+df.to_csv('clean_features.csv', index=False)
